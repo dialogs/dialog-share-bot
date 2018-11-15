ae5d5965f8f48aa48bf1b44cfbd516f0a0c859fb
Esteban Diez Laiz <esteban.diezlaiz@gmail.com>
2018-11-15 03:17:52 +0100

    removed token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         try {
             //TODO change values
             String shareBotName = "SHARE BOT BOB";
-            String myToken = "e340f0c551bc8d2268f30e1c0f8bf8627be9997b";
+            String myToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
             new ShareBot(myToken, shareBotName);
             
         } catch (InterruptedException e) {
