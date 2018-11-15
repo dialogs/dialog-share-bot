2933946e3569d0ff1e4d6a29cfeb5af173488139
Esteban Diez Laiz <esteban.diezlaiz@gmail.com>
2018-11-15 01:39:08 +0100

    share bot initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            String shareBotName = "SHARE BOT BOB";
+            String myToken = "e340f0c551bc8d2268f30e1c0f8bf8627be9997b";
+            new ShareBot(myToken, shareBotName);
+            
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
