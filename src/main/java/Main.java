d5f61a8f31cf2ceb706ebeac87da65ab70a416d9
Esteban Diez Laiz <esteban.diezlaiz@gmail.com>
2018-11-16 13:16:21 +0100

    Added shareBotTwitter account in doc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            //TODO change values
+            // TODO change values
             String shareBotName = "SHARE BOT BOB";
             String myToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-            new ShareBot(myToken, shareBotName);
-            
+            String shareBotTwitterAccount = "twitterAccount"; // just account name no @
+            new ShareBot(myToken, shareBotName, shareBotTwitterAccount);
+            // if no twitter account just:
+            // new ShareBot(myToken, shareBotName);
+            // if no name to share bot:
+            // new ShareBot(myToken);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
